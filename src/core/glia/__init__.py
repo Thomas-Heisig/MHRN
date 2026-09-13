369037d99b24ee75b7aeb967af6895d332748db0
Thomas Heisig <t_heisig@gmx.de>
2026-09-13 21:03:57 +0200

    chore: expose glial sidecars

---
+++ src/core/glia/__init__.py
@@ -0,0 +1,12 @@
+"""Experimental glial sidecar subsystems."""
+
+from .astrocyte import AstrocyteConfig, AstrocyteField
+from .microglia import MicrogliaConfig, MicrogliaField, MicrogliaProposal
+
+__all__ = [
+    "AstrocyteConfig",
+    "AstrocyteField",
+    "MicrogliaConfig",
+    "MicrogliaField",
+    "MicrogliaProposal",
+]
