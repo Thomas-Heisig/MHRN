f52c77c4938f946546d64a7d65b9917f01a90781
Thomas Heisig <t_heisig@gmx.de>
2026-09-14 17:27:58 +0200

    feat: route publication tab through new reader

--- src/dashboard/static/frontend/index.js
+++ src/dashboard/static/frontend/index.js
@@ -21,7 +21,7 @@ import { initSystemInfo } from "./modules/system-info.js";
 import { initSnapshotHistory } from "./modules/snapshot-history.js";
 import { initReviewLink } from "./modules/review-link.js";
 import { initOverviewSubtabs } from "./modules/overview-subtabs.js";
-import { initPublicationPanel } from "./modules/publication-panel.js";
+import { initPublicationPanel } from "./modules/publication-reader.js";
 import { initResearchSubtabs } from "./modules/research-subtabs.js";
 import { initNeuronModelScience } from "./modules/neuron-model-science.js";
 import { initRuntimeNeuron } from "./modules/runtime-neuron.js";
